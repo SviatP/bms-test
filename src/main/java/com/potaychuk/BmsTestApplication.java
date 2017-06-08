9275e7bd3b8a0333594c1bc7c75e35df30bfaf22
Potaychuk Sviatoslav <sviatoslavpotaychuk@gmail.com>
2017-06-08 12:34:26 +0300

    Initial commit

---
+++ src/main/java/com/potaychuk/BmsTestApplication.java
@@ -0,0 +1,12 @@
+package com.potaychuk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BmsTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BmsTestApplication.class, args);
+	}
+}
